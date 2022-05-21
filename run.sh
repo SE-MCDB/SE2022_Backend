2b41578a5594b2672f8b7cfcafd615eda816c9b5
imingx <2732834341@qq.com>
2022-05-21 22:05:24 +0800

    build: sh

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@ DIR=/home/admin
 time=`date -d "now" +%Y-%m-%d-%H-%M`
 mv $DIR/django.log $DIR/django_log/django$time.log
 
-ps -aux | grep manage.py | grep -v grep | awk '{print $2}' | xargs kill -9
+PID=`ps -aux | grep manage.py | grep -v grep | awk '{print $2}'` 
 
+if [ -n "$a" ]; then
+	echo $a | xargs kill -9
+fi
 echo "running backend"
 nohup python manage.py runserver 0.0.0.0:8000> $DIR/django.log 2>&1 & exit
