57ab6585ff4a54bd0c23b25d7c715945586859f6
gmm <2732834341@qq.com>
2022-06-06 18:39:57 +0800

    none

--- core/api/user.py
+++ core/api/user.py
@@ -176,15 +176,17 @@ def change_user_info(request: HttpRequest):
     institution = data.get('institution')
     username = data.get('name')
     email = data.get('mail')
-    
+    user = User.objects.filter(pk=pid).first()
+
     if User.objects.filter(pk=pid).exists() is False:
         return failed_api_response(ErrorCode.INVALID_REQUEST_ARGS,'Your required user to change is not found!')
-    if User.objects.filter(username=username).exists() and len(User.objects.filter(username=username)) > 1:
+
+    if username != user.username and user.objects.filter(username=username).exists():
         return failed_api_response(ErrorCode.ITEM_ALREADY_EXISTS, "Username conflicted.")
-    if User.objects.filter(email=email).exists() and len(User.objects.filter(username=username)) > 1:
+
+    if email != user.email and User.objects.filter(email=email).exists():
         return failed_api_response(ErrorCode.ITEM_ALREADY_EXISTS, "Email conflicted.")
         
-    user = User.objects.filter(pk=pid).first()
     user.username = username
     user.institution = institution
     user.email = email
